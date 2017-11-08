ffb2273b12ae017e6b23ea54dfd5ff5cad00206f
David Dollar <david@dollar.io>
2017-11-08 16:11:39 -0500

    autoscaling

--- resource-environment.sh
+++ resource-environment.sh
@@ -9,4 +9,4 @@ stack=$1
 
 [ -z $stack ] && die "usage: resource-environmnent.sh <stack-name>"
 \
-aws cloudformation describe-stacks --stack-name $stack | jq -r '.Stacks[0].Outputs[] | [(.OutputKey | gsub("(?<pre>.)(?<up>[A-Z])";"\(.pre)_\(.up)") | ascii_upcase), .OutputValue] | join("=")'
+aws cloudformation describe-stacks --stack-name $stack | jq -r '.Stacks[0].Outputs[] | [(.OutputKey | gsub("(?<pre>.)(?<up>[A-Z])";"\(.pre)_\(.up)") | ascii_upcase), .OutputValue] | join("=")' | sort
